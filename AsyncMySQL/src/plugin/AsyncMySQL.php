d98374ab54b352d0fbbedbdd3ab184f8c609e7a2
Ian Lee <35417717+Security-Development@users.noreply.github.com>
2023-07-11 23:55:30 +0900

    Update AsyncMySQL.php

--- AsyncMySQL/src/plugin/AsyncMySQL.php
+++ AsyncMySQL/src/plugin/AsyncMySQL.php
@@ -14,21 +14,6 @@ class AsyncMySQL extends PluginBase {
 
     public function onEnable() : void {
         $this->getServer()->getPluginManager()->enablePlugin($this);
-
-        /*
-
-        $mysql = new Mysql();
-        $mysql->bindTo(new Connect("localhost", "root", "lee12345", "mysql"));
-
-        $mysql->setQuery(["SHOW PROCESSLIST;"]);
-        $result = $mysql->recv();
-        //var_dump($result);
-
-        $mysql->setQuery(["SHOW TABLES;", "SHOW TABLES;"]);
-        $result = $mysql->recv();
-        var_dump($result[0][0]->Tables_in_mysql);
-        echo $result[0][0]->Tables_in_mysql."\n";
-        */
     }
 
     public static function connectToMySQL(string $host, string $username, string $password, string $database) {
